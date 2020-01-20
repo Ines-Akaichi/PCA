8d58b074769916c003fb1ed85c59f5c51a853764
Akaichi Ines <akaichi_ines@outlook.com>
2020-01-20 09:24:29 +0100

    rm job

--- Jobs/DIM_DATE_EXAM_0.1/DIM_DATE_EXAM/DIM_DATE_EXAM_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-ROOT_PATH=`pwd`
-java -Dtalend.component.manager.m2.repository=$ROOT_PATH/../lib -Xms1024M -Xmx4096M -cp .:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/dom4j-1.6.1.jar:$ROOT_PATH/../lib/external_sort.jar:$ROOT_PATH/../lib/log4j-1.2.17.jar:$ROOT_PATH/../lib/postgresql-42.2.5.jar:$ROOT_PATH/../lib/talend_file_enhanced_20070724.jar:$ROOT_PATH/../lib/talendcsv.jar:$ROOT_PATH/dim_date_exam_0_1.jar: stagepca.dim_date_exam_0_1.DIM_DATE_EXAM  --context=Default "$@"
