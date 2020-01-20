11bfa1a96b4b10a5d488e270925e42c0b802b79e
Akaichi Ines <akaichi_ines@outlook.com>
2020-01-20 09:36:16 +0100

    rm job

--- Jobs/FT_CLINICAL_MEASURES_2_0.1/FT_CLINICAL_MEASURES_2/FT_CLINICAL_MEASURES_2_run.ps1
+++
@@ -1,3 +0,0 @@
-$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-cd $fileDir
-java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms1024M' '-Xmx4096M' -cp '.;../lib/routines.jar;../lib/advancedPersistentLookupLib-1.2.jar;../lib/commons-collections-3.2.2.jar;../lib/dom4j-1.6.1.jar;../lib/jboss-serialization.jar;../lib/log4j-1.2.17.jar;../lib/postgresql-42.2.5.jar;../lib/talend_file_enhanced_20070724.jar;../lib/talendcsv.jar;../lib/trove.jar;ft_clinical_measures_2_0_1.jar;' stagepca.ft_clinical_measures_2_0_1.FT_CLINICAL_MEASURES_2  --context=Default %*
--- Jobs/FT_CLINICAL_MEASURES_2_0.1/FT_CLINICAL_MEASURES_2/FT_CLINICAL_MEASURES_2_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-ROOT_PATH=`pwd`
-java -Dtalend.component.manager.m2.repository=$ROOT_PATH/../lib -Xms1024M -Xmx4096M -cp .:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/advancedPersistentLookupLib-1.2.jar:$ROOT_PATH/../lib/commons-collections-3.2.2.jar:$ROOT_PATH/../lib/dom4j-1.6.1.jar:$ROOT_PATH/../lib/jboss-serialization.jar:$ROOT_PATH/../lib/log4j-1.2.17.jar:$ROOT_PATH/../lib/postgresql-42.2.5.jar:$ROOT_PATH/../lib/talend_file_enhanced_20070724.jar:$ROOT_PATH/../lib/talendcsv.jar:$ROOT_PATH/../lib/trove.jar:$ROOT_PATH/ft_clinical_measures_2_0_1.jar: stagepca.ft_clinical_measures_2_0_1.FT_CLINICAL_MEASURES_2  --context=Default "$@"
